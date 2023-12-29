4aa92cab73ebbeee7712dcc24193cfcf913b55db
pallavivemuri <124231023+pallavivemuri@users.noreply.github.com>
2023-12-29 11:00:43 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import streamlit as st
-st.set_page_config(page_title='cats')st.markdown("Types of cats")
+st.set_page_config(page_title='cats')
+st.markdown("Types of cats")
 st.write("persian Cat")
 st.write("white Cat")
                    
