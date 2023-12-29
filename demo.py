8454ea94176326a8b5b3195a0edcd0a90574a08f
pallavivemuri <124231023+pallavivemuri@users.noreply.github.com>
2023-12-29 11:31:25 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("Types of cats")
 st.write("persian Cat")
-st.image("C:\Users\PALLAVI\Desktop\persian.jpg")
+
 st.write("white Cat")
+
                    
