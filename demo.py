7ba21411bc7350b5909bb00bc9784143313e4369
pallavivemuri <124231023+pallavivemuri@users.noreply.github.com>
2023-12-29 12:14:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ st.markdown("Types of cats")
 col1,col2=st.columns(2)
 with col1:
   st.write("persian Cat")
-  st.image("./kitty.jpg",caption="Persian Cat",width=300,use_column_width=True))
+  st.image("./kitty.jpg",caption="Persian Cat",width=300,use_column_width=True)
   st.write("persian cat is cute")
 with col2:
   st.write("white Cat")
